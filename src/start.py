cf418425595b87a338a6f16fb8176dc1ee3d3352
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-12-05 22:32:17 +0100

    variables

--- src/start.py
+++ src/start.py
@@ -1,4 +1,4 @@
-# Wieloliniowe stringi
+# Wieloliniowe stringi, można używać zarówn0 " i '
 
 print('''My contact:
 Email: your_name@gmail.com
@@ -7,3 +7,14 @@ Phone: +xx-1234-5678''')
 print("""My contact:
 Email: your_name@gmail.com
 Phone: +xx-1234-5678""")
+
+# typy - string, int, float
+# type() - zwraca typ argumentu
+print(type(100))
+
+# definiowanie zmiennej
+city = 'Katowice'
+print(city)
+
+message = "Good Evening!"
+message = 1009
